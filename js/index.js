d952238f9db2ce6f80edde2b0e28479feaa22489
Thiago Henrique <thiagohcfidencio@gmail.com>
2023-03-01 18:58:38 -0300

    Starting pokedex

--- js/index.js
+++ js/index.js
@@ -1,14 +1,45 @@
+/* document tags*/
+const container = document.querySelector('.container')
 
+/* main variables*/ 
+let pokemonNumber = 1
+let URL = 'https://pokeapi.co/api/v2/pokemon/' + pokemonNumber
 
-// axios.get('https://api.github.com/users/ythiago03')
-// .then(response => console.log(response.data))
-
-const URL = 'https://pokeapi.co/api/v2/pokemon/347'
-
+/* getting and rendering pokemons */
 const getPokemon = async (url) => {
     const getPoke  = await axios.get(url)
     const dataPoke = getPoke.data
-    console.log(dataPoke);
+    return dataPoke
 }
 
-getPokemon(URL)
+const changePokemon = () => {
+    for(let i = 0; i < pokemonImgs.length; i++){
+        pokemonNumber = i + 1
+        URL = 'https://pokeapi.co/api/v2/pokemon/' + pokemonNumber
+        renderPokemon(URL = 'https://pokeapi.co/api/v2/pokemon/' + pokemonNumber, i)
+    }
+}
+
+const renderPokemon = async (url, currentNumber) => {
+    const pokemon = await getPokemon(URL) 
+    console.log(pokemon)
+    let pokemonURL = pokemon.sprites.versions["generation-v"]["black-white"].animated.front_default
+    pokemonImgs[currentNumber].setAttribute('src', pokemonURL)
+}
+
+/* creating imgs */
+
+const createImg = () => {
+    
+    for(let i = 0; i <= 55; i++){
+        container.innerHTML += `
+        <img class="pokemon" src="" alt="Pokemon ${i + 1}">
+        `
+    }
+}
+
+//createImg()
+
+const pokemonImgs = document.querySelectorAll('.pokemon')
+
+changePokemon()
