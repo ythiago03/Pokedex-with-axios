96414da3ec9797b366b633ba3c40f0894f88091c
Thiago Henrique <thiagohcfidencio@gmail.com>
2023-03-02 17:47:03 -0300

    Finish -  Pokedex With Axios

--- js/index.js
+++ js/index.js
@@ -22,24 +22,61 @@ const changePokemon = () => {
 
 const renderPokemon = async (currentNumber) => {
     const pokemon = await getPokemon(URL) 
-    console.log(pokemon)
+    
+    let attributes = pokemon.types
+    for(let i = 0; i < attributes.length; i++){
+        //console.log(attributes[i])
+        pokemonAttributes[currentNumber].innerHTML += `
+         <div class="attribute att-${attributes[i].type.name}">${attributes[i].type.name}</div>
+        `
+    }
+
     let pokemonURL = pokemon.sprites.versions["generation-v"]["black-white"].animated.front_default
+    let pokeName = pokemon.name
+    let pokeId = pokemon.id
+
+
     pokemonImgs[currentNumber].setAttribute('src', pokemonURL)
+    pokemonName[currentNumber].innerHTML = pokeName
+
+    if(pokeId < 10){
+        pokeId = '#000' + pokeId
+    }
+    if(pokeId < 100){
+        pokeId = '#00' + pokeId
+    }
+    if(pokeId < 1000){
+        pokeId = '#0' + pokeId
+    }
+
+    pokemonId[currentNumber].innerHTML = pokeId
 }
 
 /* creating imgs */
 
-const createImg = () => {
+const createCards = () => {
     
-    for(let i = 0; i <= 55; i++){
+    for(let i = 0; i <= 49; i++){
         container.innerHTML += `
-        <img class="pokemon" src="" alt="Pokemon ${i + 1}">
+        <div class="card">
+            <div class="card-img">
+                <img class="pokemon" src="" alt="Pokemon ${i + 1}">
+            </div>
+            <span class="card-id">#0001</span>
+            <h2 class="card-name">Bulbasaur</h2>
+            <div class="attributes">
+                
+            </div>
+        </div>
         `
     }
 }
 
-//createImg()
+createCards()
 
 const pokemonImgs = document.querySelectorAll('.pokemon')
+const pokemonName = document.querySelectorAll('.card-name')
+const pokemonId = document.querySelectorAll('.card-id')
+const pokemonAttributes = document.querySelectorAll('.attributes')
 
 changePokemon()
